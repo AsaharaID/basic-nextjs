4d09d8941f98d645e1da2a09079f7d83a8e59da7
Muhamad Syah Reza <49017831+AsaharaID@users.noreply.github.com>
2022-01-06 16:29:12 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Avec<a href="https://nextjs.org"> Pleseir</a>
         </h1>
 
         <p className={styles.description}>
