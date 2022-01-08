621783126cdabbad9625a858cb3a6410bb5ed501
Muhamad Syah Reza <49017831+AsaharaID@users.noreply.github.com>
2022-01-08 15:23:51 +0700

    commit

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,19 +1,31 @@
+/** @format */
 
+import Head from "next/head";
 import { ReactNode } from "react";
 import Footer from "../Footer";
 import Header from "../header";
-import styles from "./Layout.module.css"
+import styles from "./Layout.module.css";
 
-interface LayoutProps{
-    children: ReactNode;
+interface LayoutProps {
+	children: ReactNode;
+	pageTitle: String;
 }
 export default function Layout(props: LayoutProps) {
-    const { children } = props;
-    return (
-        <div className={styles.container}>
-            <Header />
-            <div className={styles.content}>{children}</div>
-            <Footer />
-        </div>
-    )
+	const { children, pageTitle } = props;
+	return (
+		<>
+			<Head>
+				<title>
+					NextJS Basic | {""}
+					{pageTitle}
+				</title>
+				<meta name="description" content="Website NextJS Basic"></meta>
+			</Head>
+			<div className={styles.container}>
+				<Header />
+				<div className={styles.content}>{children}</div>
+				<Footer />
+			</div>
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 /** @format */
 
+import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/header";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
 	return (
-		<Layout>
+		<Layout pageTitle="Home Page">
+			<Image src="/Champions_logo.png" width={200} height={200} alt="profile" />
 			<h1 className={styles["title-homepage"]}>Avec Plaisir</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adip</p>
 		</Layout>
 	);
 }
